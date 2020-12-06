89d2a54bc485d8355a312e62aa234fc9afd4ceba
matsjoonas <mj@codelight.eu>
2020-12-06 12:47:41 +0200

    Refactor day6_2

--- day6/day6_2.js
+++ day6/day6_2.js
@@ -20,19 +20,15 @@ fs.readFile('./input.txt', (e, data) => {
     return groups;
   }
 
-  function intersection(array) {
-    return array.reduce((acc, cur) => {
-      const curAsArray = cur.split('');
-      if (!acc) {
-        return curAsArray;
-      }
-      return acc.filter(x => curAsArray.includes(x));
-    }, null);
+  function intersection(str1, str2) {
+    return str1
+      .split('')
+      .filter(x => str2.split('').includes(x))
+      .join('');
   }
 
-
   const count = getGroups(input).reduce((total, group) => {
-    return total + intersection(group).length;
+    return total + group.reduce(intersection, group[0]).length;
   }, 0);
 
   console.log(count);
