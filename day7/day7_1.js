f72228d42e7ddd5e8eae483e4290caea85e7d38a
matsjoonas <mj@codelight.eu>
2020-12-07 12:13:03 +0200

    Add day7 solutions

--- day7/day7_1.js
+++ day7/day7_1.js
@@ -2,19 +2,56 @@ const AocSuite = require('../util/AocSuite');
 const suite = new AocSuite({
   solver,
   testInputsDir: 'testInputs1',
-  expectedTestAnswers: [],
+  expectedTestAnswers: [4],
 });
 
 //suite.performance();
-//suite.test();
+suite.test();
 suite.solve();
 
 function solver(data) {
   const input = data.toString().trim().split('\r\n');
+  const processed = input.map(line => {
+    let processedLine = line.split('.').join('')
+      .split(',').join('')
+      .split('contain').join('')
+      .split('bags').join('|')
+      .split('bag').join('|')
+      .split('  ').join(' ')
+      .split('|')
+      .map((item, idx) => {
+        let newItem = item.trim();
+        if (idx > 0) {
+          newItem = newItem.substr(2);
+        }
+        return newItem;
+      })
+      .filter(item => item !== '' && item !== ' other');
 
-  console.log(input);
+    return {
+      name: processedLine[0],
+      contents: processedLine.slice(1),
+    };
+  }).filter(item => item.name !== 'shiny gold' && item.contents.length !== 0)
+    .reduce((acc, cur) => {
+      acc[cur.name] = cur.contents;
+      return acc;
+    }, {});
 
-  return null;
+
+  const possibleChildren = (name) => {
+    let contents = processed[name] || [];
+    const colors = [...contents];
+
+    contents.forEach(color => colors.push(...possibleChildren(color)));
+
+    return colors;
+  };
+
+  return Object
+    .keys(processed)
+    .filter((key) => possibleChildren(key).includes('shiny gold'))
+    .length;
 }
 
 
--- day7/day7_2.js
+++ day7/day7_2.js
@@ -2,7 +2,7 @@ const AocSuite = require('../util/AocSuite');
 const suite = new AocSuite({
   solver,
   testInputsDir: 'testInputs2',
-  expectedTestAnswers: [],
+  expectedTestAnswers: [126, 32],
 });
 
 //suite.performance();
@@ -10,11 +10,42 @@ const suite = new AocSuite({
 suite.solve();
 
 function solver(data) {
-  const input = data.toString().trim().split('\r\n');
+  const input = data.toString().trim().split('\r\n').map(line => {
+    let processedLine = line.split('.').join('')
+      .split(',').join('')
+      .split('contain').join('')
+      .split('bags').join('|')
+      .split('bag').join('|')
+      .split('  ').join(' ')
+      .split('|')
+      .filter(item => item !== '' && item.trim() !== 'no other')
+      .map((item, idx) => {
+        let newItem = item.trim();
+        if (idx > 0) {
+          newItem = [newItem.substr(2), parseInt(newItem.substr(0, 1))];
+        }
+        return newItem;
+      });
 
-  console.log(input);
+    return {
+      name: processedLine[0],
+      contents: processedLine.slice(1),
+    };
+  }).reduce((acc, cur) => {
+      acc[cur.name] = cur.contents;
+      return acc;
+    }, {});
 
-  return null;
+  const getNrOfChildren = (name, parentCount) => {
+    let children = input[name] || [];
+    let nrOfChildren = children.reduce((acc, cur) => {
+      return acc + cur[1];
+    }, 0);
+    children.forEach(child => nrOfChildren += getNrOfChildren(child[0], child[1]));
+    return (parentCount || 1) * nrOfChildren;
+  };
+
+  return getNrOfChildren('shiny gold');
 }
 
 
