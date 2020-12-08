367b281d0183b2c67cfac3408c23ee93130811d9
matsjoonas <mj@codelight.eu>
2020-12-08 13:13:08 +0200

    Add day8

--- day8/day8_1.js
+++ day8/day8_1.js
@@ -2,17 +2,44 @@ const AocSuite = require('../util/AocSuite');
 const suite = new AocSuite({
   solver,
   testInputsDir: 'testInputs1',
-  expectedTestAnswers: [],
+  expectedTestAnswers: [5],
 });
 
 
 function solver(data) {
-  const input = data.toString().trim().split('\r\n');
+  const input = data.toString().trim().split('\r\n')
+    .map(line => {
+      const parsed = line.split(' ');
+      parsed[1] = parseInt(parsed[1]);
+      return parsed;
+    });
 
-  return null;
+  const executedInstructions = [];
+  let accumulator = 0;
+  let idx = 0;
+  while (true) {
+    if (executedInstructions.find(item => item === idx)) {
+      break;
+    }
+    executedInstructions.push(idx);
+
+    const instruction = input[idx][0];
+    const argument = input[idx][1];
+
+    if (instruction === 'nop') {
+      idx++
+    } else if (instruction === 'acc') {
+      accumulator += argument;
+      idx++
+    } else if (instruction === 'jmp') {
+      idx += argument;
+    }
+  }
+
+  return accumulator;
 }
 
 
-//suite.performance();
 suite.test();
 suite.solve();
+//suite.performance();
--- day8/day8_2.js
+++ day8/day8_2.js
@@ -2,16 +2,105 @@ const AocSuite = require('../util/AocSuite');
 const suite = new AocSuite({
   solver,
   testInputsDir: 'testInputs2',
-  expectedTestAnswers: [],
+  expectedTestAnswers: [8],
 });
 
-function solver(data) {
-  const input = data.toString().trim().split('\r\n');
+class Computer {
+  constructor() {
+    this.acc = 0;
+    this.ptr = 0;
+    this.executed = [];
+    this.status = null;
+    this.ops = {
+      nop: () => this.ptr++,
+      acc: (arg) => {
+        this.acc += arg;
+        this.ptr++;
+      },
+      jmp: (arg) => this.ptr += arg,
+    }
+    this.STATUS_CODES = {
+      error: 'error',
+      terminated: 'terminated',
+    };
+  }
 
-  return null;
+  reset() {
+    this.acc = 0;
+    this.ptr = 0;
+    this.executed = [];
+    this.status = null;
+  }
+
+  setStatus(code, msg) {
+    this.status = {
+      code,
+      msg,
+    }
+  }
+
+  run(instructions) {
+    this.reset();
+    const _self = this;
+
+    function shouldContinue() {
+      let decision = true;
+      if (_self.ptr > instructions.length - 1) {
+        decision = false;
+        _self.setStatus(_self.STATUS_CODES.terminated, `ptr: ${_self.ptr} points to a nonexistent instruction`);
+      } else if (_self.executed[_self.ptr]) {
+        decision = false;
+        _self.setStatus(_self.STATUS_CODES.error, `instruction ${_self.ptr} has already been executed`);
+      }
+      return decision;
+    }
+
+    while (shouldContinue()) {
+      _self.executed[_self.ptr] = true;
+      const [opcode, arg] = instructions[_self.ptr];
+      _self.ops[opcode](arg);
+    }
+
+    return {
+      status: _self.status,
+      acc: _self.acc,
+    }
+  }
 }
 
-//suite.performance();
-suite.test();
-suite.solve();
+function solver(data) {
+  const instructions = data.toString().trim().split('\r\n')
+    .map(line => {
+      const formatted = line.split(' ');
+      return [formatted[0], parseInt(formatted[1])];
+    });
 
+  const gameConsole = new Computer();
+
+  for (let i = 0; i < instructions.length; i++) {
+    if (instructions[i][0] === 'acc') {
+      continue;
+    }
+
+    let modifiedInstructions = [...instructions];
+
+    let newInstruction = [...modifiedInstructions[i]]
+    if (newInstruction[0] === 'jmp') {
+      newInstruction[0] = 'nop';
+    } else if (newInstruction[0] === 'nop') {
+      newInstruction[0] = 'jmp';
+    }
+    modifiedInstructions[i] = newInstruction;
+
+    const result = gameConsole.run(modifiedInstructions);
+
+    if (result.status.code === gameConsole.STATUS_CODES.terminated) {
+      return result.acc;
+    }
+  }
+}
+
+
+//suite.test();
+suite.solve();
+//suite.performance();
