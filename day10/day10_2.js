81e5d77fa9f8605dc2d9ca177bfecce3357f1d44
matsjoonas <mj@codelight.eu>
2020-12-09 11:08:11 +0200

    Add boilerplate for day10

---
+++ day10/day10_2.js
@@ -0,0 +1,18 @@
+const AocSuite = require('../util/AocSuite');
+
+function solver(data) {
+  const input = data.toString().trim().split('\r\n');
+
+
+  return null;
+}
+
+const suite = new AocSuite({
+  solver,
+  testInputsDir: 'testInputs2',
+  expectedTestAnswers: [null],
+});
+
+//suite.test();
+suite.solve();
+//suite.performance();
