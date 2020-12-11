ffd91c5f3c908eb476eb717c6fc7773296a09e2a
matsjoonas <mj@codelight.eu>
2020-12-11 11:26:38 +0200

    Add day11 brutefroce solutions

--- day11/day11_1.js
+++ day11/day11_1.js
@@ -1,11 +1,71 @@
 const AocSuite = require('../util/AocSuite');
 
 function solver(data) {
-  const input = data.toString().trim().split('\r\n');
+  const layout = data.toString().trim().split('\r\n')
+    .map(row => row.split(''));
 
-  console.log(input);
+  function convertToString(layout) {
+    return layout.map(row => {
+      return row.join();
+    }).join();
+  }
 
-  return input;
+  function areLayoutsEqual(a, b) {
+    return convertToString(a) === convertToString(b);
+  }
+
+  function getAdjacentUnits(layout, rowIdx, unitIdx) {
+    let adjacentUnits = '';
+    let topRow = layout[rowIdx - 1] || [];
+    let middleRow = layout[rowIdx] || [];
+    let bottomRow = layout[rowIdx + 1] || [];
+    adjacentUnits += topRow[unitIdx - 1] || '' // top left
+    adjacentUnits += topRow[unitIdx] || '' // top center
+    adjacentUnits += topRow[unitIdx + 1] || '' // top right
+    adjacentUnits += middleRow[unitIdx - 1] || '' // left
+    adjacentUnits += middleRow[unitIdx + 1] || '' // right
+    adjacentUnits += bottomRow[unitIdx - 1] || '' // bottom left
+    adjacentUnits += bottomRow[unitIdx] || '' // bottom center
+    adjacentUnits += bottomRow[unitIdx + 1] || '' // bottom right
+    return adjacentUnits;
+  }
+
+  function getNexLayout(layout) {
+    let newLayout = [];
+    layout.forEach((row, rowIdx) => {
+      if (!newLayout[rowIdx]) newLayout[rowIdx] = [];
+
+      row.forEach((unit, unitIdx) => {
+        let modifiedUnit = unit;
+        if (unit === 'L') {
+          // check for adjacent occupied seats
+          if (!getAdjacentUnits(layout, rowIdx, unitIdx).includes('#')) {
+            modifiedUnit = '#';
+          }
+        } else if (unit === '#') {
+          if (getAdjacentUnits(layout, rowIdx, unitIdx).split("#").length-1 >= 4) {
+            modifiedUnit = 'L';
+          }
+        }
+        newLayout[rowIdx].push(modifiedUnit);
+      });
+    });
+
+    return newLayout;
+  }
+
+  const frames = [];
+  let prevLayout;
+  let nextLayout;
+  do {
+    prevLayout = frames[frames.length - 1] || layout;
+    nextLayout = getNexLayout(prevLayout);
+    frames.push(nextLayout);
+  } while (!areLayoutsEqual(prevLayout, nextLayout));
+  
+
+  return convertToString(frames[frames.length - 1]).split("#").length-1;
+
 }
 
 const suite = new AocSuite({
--- day11/day11_2.js
+++ day11/day11_2.js
@@ -1,11 +1,112 @@
 const AocSuite = require('../util/AocSuite');
 
+
 function solver(data) {
-  const input = data.toString().trim().split('\r\n');
+  const layout = data.toString().trim().split('\r\n')
+    .map(row => row.split(''));
 
-  console.log(input);
+  function convertToString(layout) {
+    return layout.map(row => {
+      return row.join();
+    }).join();
+  }
 
-  return input;
+  function areLayoutsEqual(a, b) {
+    return convertToString(a) === convertToString(b);
+  }
+
+
+  function visionCreator(layout, initialPosition) {
+    function look(direction, position = initialPosition) {
+      let rowMod = 0;
+      let unitMod = 0;
+      let directionX = direction[0];
+      let directionY = direction[1];
+      if (directionX === 'left') {
+        unitMod = -1;
+      }
+      if (directionX === 'right') {
+        unitMod = 1;
+      }
+      if (directionY === 'up') {
+        rowMod = -1;
+      }
+      if (directionY === 'down') {
+        rowMod = 1;
+      }
+
+      const nextPosition = {
+        row: position.row + rowMod,
+        unit: position.unit + unitMod,
+      }
+      let nextRow = layout[nextPosition.row] || [];
+      let lookingAt = nextRow[nextPosition.unit];
+      if (lookingAt === '.') {
+        return look(direction, nextPosition);
+      } else {
+        return lookingAt || '';
+      }
+    }
+
+    return {
+      look,
+    }
+  }
+
+  function getVisibleUnits(layout, row, unit) {
+    const vision = visionCreator(layout, {
+      row,
+      unit,
+    });
+
+    return vision.look(['', 'up']) +
+      vision.look(['left', 'up']) +
+      vision.look(['left', '']) +
+      vision.look(['left', 'down']) +
+      vision.look(['', 'down']) +
+      vision.look(['right', 'down']) +
+      vision.look(['right', '']) +
+      vision.look(['right', 'up']);
+
+  }
+
+  function getNexLayout(layout) {
+    let newLayout = [];
+    layout.forEach((row, rowIdx) => {
+      if (!newLayout[rowIdx]) newLayout[rowIdx] = [];
+
+      row.forEach((unit, unitIdx) => {
+        let modifiedUnit = unit;
+
+        const visibleUnits = getVisibleUnits(layout, rowIdx, unitIdx);
+        if (unit === 'L') {
+          // check for adjacent occupied seats
+          if (!visibleUnits.includes('#')) {
+            modifiedUnit = '#';
+          }
+        } else if (unit === '#') {
+          if (visibleUnits.split("#").length - 1 >= 5) {
+            modifiedUnit = 'L';
+          }
+        }
+        newLayout[rowIdx].push(modifiedUnit);
+      });
+    });
+
+    return newLayout;
+  }
+
+
+  const frames = [];
+  let prevLayout;
+  let nextLayout;
+  do {
+    prevLayout = frames[frames.length - 1] || layout;
+    nextLayout = getNexLayout(prevLayout);
+    frames.push(nextLayout);
+  } while (!areLayoutsEqual(prevLayout, nextLayout));
+
+  return convertToString(frames[frames.length - 1]).split("#").length-1;
 }
 
 const suite = new AocSuite({
