cf04f775cfbf9812a03e18728c0bc4bc4ed1e8d9
matsjoonas <mj@codelight.eu>
2020-12-11 14:33:09 +0200

    Refactor day11_1 to also use the new Grid class

--- day11/day11_1.js
+++ day11/day11_1.js
@@ -1,4 +1,5 @@
 const AocSuite = require('../util/AocSuite');
+const Grid = require('../util/Grid');
 
 function solver(data) {
   const layout = data.toString().trim().split('\r\n')
@@ -14,36 +15,42 @@ function solver(data) {
     return convertToString(a) === convertToString(b);
   }
 
-  function getAdjacentUnits(layout, rowIdx, unitIdx) {
-    let adjacentUnits = '';
-    let topRow = layout[rowIdx - 1] || [];
-    let middleRow = layout[rowIdx] || [];
-    let bottomRow = layout[rowIdx + 1] || [];
-    adjacentUnits += topRow[unitIdx - 1] || '' // top left
-    adjacentUnits += topRow[unitIdx] || '' // top center
-    adjacentUnits += topRow[unitIdx + 1] || '' // top right
-    adjacentUnits += middleRow[unitIdx - 1] || '' // left
-    adjacentUnits += middleRow[unitIdx + 1] || '' // right
-    adjacentUnits += bottomRow[unitIdx - 1] || '' // bottom left
-    adjacentUnits += bottomRow[unitIdx] || '' // bottom center
-    adjacentUnits += bottomRow[unitIdx + 1] || '' // bottom right
-    return adjacentUnits;
+  const lookingDirections = [
+    [0, -1],
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+  ];
+
+  function getAdjacentUnits(grid, position, lookingDirections) {
+    return lookingDirections.reduce((acc, direction) => {
+      return acc + grid.look(direction, position);
+    }, '');
   }
 
   function getNexLayout(layout) {
     let newLayout = [];
+    const grid = new Grid({
+      grid: layout,
+      lineOfSightBlockers: ['#', 'L', '.'],
+    });
     layout.forEach((row, rowIdx) => {
       if (!newLayout[rowIdx]) newLayout[rowIdx] = [];
 
       row.forEach((unit, unitIdx) => {
         let modifiedUnit = unit;
+        const adjacentUnits = getAdjacentUnits(grid, [unitIdx, rowIdx], lookingDirections);
         if (unit === 'L') {
           // check for adjacent occupied seats
-          if (!getAdjacentUnits(layout, rowIdx, unitIdx).includes('#')) {
+          if (!adjacentUnits.includes('#')) {
             modifiedUnit = '#';
           }
         } else if (unit === '#') {
-          if (getAdjacentUnits(layout, rowIdx, unitIdx).split("#").length-1 >= 4) {
+          if (adjacentUnits.split("#").length-1 >= 4) {
             modifiedUnit = 'L';
           }
         }
@@ -62,7 +69,7 @@ function solver(data) {
     nextLayout = getNexLayout(prevLayout);
     frames.push(nextLayout);
   } while (!areLayoutsEqual(prevLayout, nextLayout));
-  
+
 
   return convertToString(frames[frames.length - 1]).split("#").length-1;
 
