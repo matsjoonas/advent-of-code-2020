3c6cd5f22d3ab5a5d4c09c37ee3af014aee7d574
matsjoonas <mj@codelight.eu>
2020-12-14 11:48:46 +0200

    Add quick and dirt bruteforce solutions for day14

--- day14/day14_1.js
+++ day14/day14_1.js
@@ -1,17 +1,86 @@
 const AocSuite = require('../util/AocSuite');
+const _ = require('lodash');
+
+class Computer {
+  constructor() {
+    this.ptr = 0;
+    this.memory = [];
+    this.mask = null;
+  }
+
+  dec2bin(dec){
+    return _.padStart((dec >>> 0).toString(2), 36, '0');
+  }
+
+  applyMask(value) {
+    const mask = this.mask.split('');
+    const bin = this.dec2bin(value).split('');
+    const masked = bin.map((char, idx) => {
+      if (mask[idx] !== 'X') {
+        return mask[idx];
+      } else {
+        return char;
+      }
+    });
+    return parseInt(masked.join(''), 2);
+  }
+
+  runInstruction(instruction) {
+    const [opcode, ...rest] = instruction;
+    const ops = {
+      mem: ([address, value]) => this.memory[address] = value,
+      mask: ([value]) => this.mask = value,
+    }
+
+    let args = rest;
+    if (opcode !== 'mask') {
+      args = rest.map(arg => !isNaN(parseInt(arg, 10)) ? parseInt(arg, 10) : arg);
+    }
+
+    if (args[1] !== undefined) {
+      args[1] = this.applyMask(args[1]);
+    }
+
+    ops[opcode](args);
+  }
+
+  run(instructions) {
+    const _self = this;
+
+    function shouldContinue() {
+      return _self.ptr <= instructions.length - 1;
+    }
+
+    while (shouldContinue()) {
+      _self.runInstruction(instructions[_self.ptr])
+      _self.ptr++;
+    }
+
+    return {
+      memory: _self.memory,
+    }
+  }
+}
 
 function solver(data) {
-  const input = data.toString().trim().split('\r\n');
+  const input = data.toString().trim().split('\r\n')
+    .map(line => line.split(' = ')
+      .map(item => item.split('[')
+        .map(item => item.split(']').join(''))
+      ).reduce((acc, cur) => {
+        return acc.concat(cur);
+      }, [])
+    );
 
-  console.log(input);
+  const computer = new Computer();
 
-  return null;
+  return computer.run(input).memory.reduce((acc, cur) => acc + cur, 0);
 }
 
 const suite = new AocSuite({
   solver,
   testInputsDir: 'testInputs1',
-  expectedTestAnswers: [null],
+  expectedTestAnswers: [165],
 });
 
 //suite.test();
--- day14/day14_2.js
+++ day14/day14_2.js
@@ -1,17 +1,113 @@
 const AocSuite = require('../util/AocSuite');
+const _ = require('lodash');
+
+class Computer {
+  constructor() {
+    this.ptr = 0;
+    this.memory = {};
+    this.mask = null;
+  }
+
+  dec2bin(dec) {
+    return _.padStart((dec >>> 0).toString(2), 36, '0');
+  }
+
+  applyMask(value) {
+    const mask = this.mask.split('');
+    const bin = this.dec2bin(value).split('');
+    let floatingMultiplier = 1;
+    let maskedAddresses = [];
+    const masked = bin.map((char, idx) => {
+      if (mask[idx] === '1') {
+        return mask[idx];
+      } else {
+        return char;
+      }
+    });
+
+    maskedAddresses.push(masked);
+    bin.forEach((char, idx) => {
+      if (mask[idx] === 'X') {
+        const newAddresses = [];
+        maskedAddresses.forEach(address => {
+          const new1 = [...address];
+          const new2 = [...address];
+          new1[idx] = '0';
+          new2[idx] = '1';
+          newAddresses.push(new1);
+          newAddresses.push(new2);
+        });
+        maskedAddresses = maskedAddresses
+          .concat(newAddresses);
+      }
+    });
+
+    return maskedAddresses.map(item => item.join(''));
+  }
+
+  runInstruction(instruction) {
+    const [opcode, ...rest] = instruction;
+    const ops = {
+      mem: ([address, value]) => {
+        const addresses = this.applyMask(address);
+        addresses.forEach(address => {
+          this.memory[address] = value;
+        });
+      },
+      mask: ([value]) => this.mask = value,
+    }
+
+    let args = rest;
+    if (opcode !== 'mask') {
+      args = rest.map(arg => !isNaN(parseInt(arg, 10)) ? parseInt(arg, 10) : arg);
+    }
+
+    ops[opcode](args);
+  }
+
+  run(instructions) {
+    const _self = this;
+
+    function shouldContinue() {
+      return _self.ptr <= instructions.length - 1;
+    }
+
+    while (shouldContinue()) {
+      _self.runInstruction(instructions[_self.ptr])
+      _self.ptr++;
+    }
+
+    return {
+      memory: _self.memory,
+    }
+  }
+}
 
 function solver(data) {
-  const input = data.toString().trim().split('\r\n');
+  const input = data.toString().trim().split('\r\n')
+    .map(line => line.split(' = ')
+      .map(item => item.split('[')
+        .map(item => item.split(']').join(''))
+      ).reduce((acc, cur) => {
+        return acc.concat(cur);
+      }, [])
+    );
 
-  console.log(input);
+  const computer = new Computer();
+  const result = computer.run(input).memory;
 
-  return null;
+  let sum = 0;
+  for (const [key, value] of Object.entries(result)) {
+    sum += value;
+  }
+
+  return sum;
 }
 
 const suite = new AocSuite({
   solver,
   testInputsDir: 'testInputs2',
-  expectedTestAnswers: [null],
+  expectedTestAnswers: [208],
 });
 
 //suite.test();
