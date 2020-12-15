13a77714689be129c1788ffa9e40d44a80ff1aa0
matsjoonas <mj@codelight.eu>
2020-12-15 10:25:42 +0200

    Add bruteforce solution to day15

--- day15/day15_1.js
+++ day15/day15_1.js
@@ -1,16 +1,30 @@
 const AocSuite = require('../util/AocSuite');
 
 function solver(data) {
-  const input = data.toString().trim().split('\r\n');
+  const input = data.toString().trim().split('\r\n')
+    .map(line => line.split(',').map(item => parseInt(item, 10)) );
 
-  console.log(input);
-  return null;
+  const memory = [...input[0]];
+  for (let i = memory.length - 1; i < 2020; i++) {
+    const lastNumber = memory.pop();
+    const prevIndex = memory.lastIndexOf(lastNumber);
+    memory.push(lastNumber);
+
+    if (prevIndex === -1) {
+      memory.push(0);
+    } else {
+      memory.push(i - prevIndex);
+    }
+  }
+
+  return memory[memory.length - 2];
 }
 
 const suite = new AocSuite({
   solver,
   testInputsDir: 'testInputs1',
-  expectedTestAnswers: [null],
+  expectedTestAnswers: [436],
+  //expectedTestAnswers: [436, 1, 10, 27, 78, 438, 1836],
 });
 
 //suite.test();
--- day15/day15_2.js
+++ day15/day15_2.js
@@ -1,16 +1,37 @@
 const AocSuite = require('../util/AocSuite');
 
 function solver(data) {
-  const input = data.toString().trim().split('\r\n');
+  const input = data.toString().trim().split('\r\n')
+    .map(line => line.split(',').map(item => parseInt(item, 10)) )[0];
 
-  console.log(input);
-  return null;
+  const cache = new Map();
+  input.forEach((item, idx) => {
+    cache.set(item, idx);
+  });
+  let lastNumber;
+  for (let i = input.length - 1; i < 29999999; i++) {
+    if (lastNumber === undefined) {
+      lastNumber = input[i];
+    }
+
+    const prevIndex = cache.get(lastNumber);
+    cache.set(lastNumber, i)
+
+    let newNumber = 0;
+    if (prevIndex !== undefined) {
+      newNumber = i - prevIndex;
+    }
+    lastNumber = newNumber;
+  }
+
+
+  return lastNumber;
 }
 
 const suite = new AocSuite({
   solver,
-  testInputsDir: 'testInputs2',
-  expectedTestAnswers: [null],
+  testInputsDir: 'testInputs1',
+  expectedTestAnswers: [175594],
 });
 
 //suite.test();
