815a6fdc80a2ce9e7a2f7280994b483694e41d77
matsjoonas <mj@codelight.eu>
2020-12-22 09:54:20 +0200

    Add day22_1

--- day22/parseInput.js
+++ day22/parseInput.js
@@ -1,6 +1,9 @@
 
 function parseInput(data) {
-  return  data.toString().trim().split('\r\n');
+  return  data.toString().trim().split('\r\n\r\n')
+    .map(line => {
+      return line.split('\r\n');
+    }).map(item => item.slice(1));
 }
 
 module.exports = parseInput;
