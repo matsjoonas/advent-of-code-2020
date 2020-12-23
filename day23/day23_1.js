d973b56146f75a5fb81a2b6a0d56481eee3e7ecc
matsjoonas <mj@codelight.eu>
2020-12-23 10:20:43 +0200

    Solve day23_1

--- day23/day23_1.js
+++ day23/day23_1.js
@@ -2,17 +2,72 @@ const _ = require('lodash');
 const AocSuite = require('../util/AocSuite');
 const parseInput = require('./parseInput');
 
-function solver(data) {
-  let input = parseInput(data);
-  console.log(input);
 
-  return null;
+function spliceCups(cups, startingIdx, nrOfCups) {
+  const removedCups = [];
+  for (let i = 0; i < nrOfCups; i++) {
+    if (startingIdx > cups.length - 1) {
+      startingIdx = 0;
+    }
+    removedCups.push(cups.splice(startingIdx, 1)[0]);
+  }
+  return removedCups;
+}
+
+function findDestinationCupIndex(cups, currentCup) {
+  const scratchCups = [...cups];
+  let destinationCupLabel = scratchCups.sort((a, b) => b - a)
+    .find(cup => cup < currentCup);
+
+  if (destinationCupLabel === undefined) {
+    destinationCupLabel = scratchCups.sort((a, b) => b - a)[0];
+  }
+
+  return cups.findIndex(cup => cup === destinationCupLabel);
+}
+
+function insertCups(cups, destinationIndex, cupsToInsert) {
+  cupsToInsert.reverse().forEach(cup => {
+    cups.splice(destinationIndex, 0, cup);
+  });
+}
+
+function maybeRealignCups(cups, currentCup, currentCupOriginalIndex) {
+  const currentCupNewIndex = cups.findIndex(cup => cup === currentCup);
+  if (currentCupNewIndex !== currentCupOriginalIndex) {
+    const diff = currentCupNewIndex - currentCupOriginalIndex;
+    return [...cups, ...cups].slice(diff, diff + cups.length);
+  } else {
+    return cups;
+  }
+}
+
+
+function solver(data) {
+  let cups = parseInput(data);
+
+  let nrOfTurns = 100;
+  let currentCupIdx = 0;
+  for (let i = 0; i < nrOfTurns; i++) {
+    if (currentCupIdx > cups.length - 1) {
+      currentCupIdx = 0;
+    }
+    const currentCup = cups[currentCupIdx];
+    const movingCups = spliceCups(cups, currentCupIdx + 1, 3)
+    const destinationCupIndex = findDestinationCupIndex(cups, currentCup);
+    insertCups(cups, destinationCupIndex + 1, movingCups);
+    cups = maybeRealignCups(cups, currentCup, currentCupIdx);
+
+    currentCupIdx++;
+  }
+
+  return [...cups, ...cups].slice(cups.indexOf(1) + 1, cups.length + 1).join('');
 }
 
 const suite = new AocSuite({
   solver,
   testInputsDir: 'testInputs1',
-  expectedTestAnswers: [null],
+  expectedTestAnswers: ['67384529'],
 });
 
 //suite.test();
--- day23/parseInput.js
+++ day23/parseInput.js
@@ -1,6 +1,6 @@
 
 function parseInput(data) {
-  return  data.toString().trim().split('\r\n');
+  return  data.toString().trim().split('').map(value => parseInt(value, 10));
 }
 
 module.exports = parseInput;
