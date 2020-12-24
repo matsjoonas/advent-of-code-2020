29f3527c71d8c075d40c6c0bc1534e3642869a56
matsjoonas <mj@codelight.eu>
2020-12-24 13:57:09 +0200

    Add boilerplate for day25

---
+++ day25/day25_2.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+const AocSuite = require('../util/AocSuite');
+const parseInput = require('./parseInput');
+
+function solver(data) {
+  let input = parseInput(data);
+  console.log(input);
+  return null;
+}
+
+const suite = new AocSuite({
+  solver,
+  testInputsDir: 'testInputs2',
+  expectedTestAnswers: [null],
+});
+
+suite.test();
+//suite.solve();
+//suite.performance();
